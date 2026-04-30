63774c591389d745d8636ec3a3744e84367de4b8
BellemManal <bellemmanel@gmail.com>
2026-04-30 22:38:52 +0200

    Update list.php

--- books/list.php
+++ books/list.php
@@ -2,20 +2,23 @@
 session_start();
 
 if(!isset($_SESSION['user_id'])){
-    header("Location: auth/login.php");
+    header("Location: ../auth/login.php");
     exit();
 }
-?>
 
-<?php
 include("../config/db.php");
-session_start();
+
 
 if($_SESSION['role'] != "librarian" && $_SESSION['role'] != "admin"){
     die("Access denied");
 }
 
-$result = $conn->query("SELECT * FROM books");
+
+$page = isset($_GET['page']) ? $_GET['page'] : 1;
+$limit = 5;
+$offset = ($page - 1) * $limit;
+
+$result = $conn->query("SELECT * FROM books LIMIT $limit OFFSET $offset");
 ?>
 
 <h2> Books List</h2>
@@ -33,9 +36,6 @@ $result = $conn->query("SELECT * FROM books");
         <th>Actions</th>
     </tr>
 
-    <a href="?page=1">1</a>
-    <a href="?page=2">2</a>
-    
     <?php while($row = $result->fetch_assoc()): ?>
     <tr>
         <td><?= $row['title'] ?></td>
@@ -43,16 +43,21 @@ $result = $conn->query("SELECT * FROM books");
         <td><?= $row['isbn'] ?></td>
         <td><?= $row['total_copies'] ?></td>
         <td><?= $row['available_copies'] ?></td>
+
         <td>
             <a href="edit.php?id=<?= $row['id'] ?>">Edit</a> |
             <a href="delete.php?id=<?= $row['id'] ?>">Delete</a>
-            <a href="../loans/borrow.php?book_id=<?= $row['id'] ?>">
-     Borrow
-</a>
         </td>
     </tr>
     <?php endwhile; ?>
 </table>
 
 <br>
-<a href="add.php"> Add New Book</a>
+
+
+<a href="?page=1">1</a>
+<a href="?page=2">2</a>
+
+<br><br>
+
+<a href="add.php">➕ Add New Book</a>
