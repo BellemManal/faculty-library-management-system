e65756b7107bfcd1a5ed734aef832b21d46ea783
BellemManal <bellemmanel@gmail.com>
2026-04-30 22:03:38 +0200

    Create return.php

---
+++ loans/return.php
@@ -0,0 +1,22 @@
+<?php
+include("../config/db.php");
+session_start();
+
+$loan_id = $_GET['loan_id'];
+
+
+$loan = $conn->query("SELECT * FROM loans WHERE id=$loan_id")->fetch_assoc();
+
+
+$conn->query("UPDATE books 
+SET available_copies = available_copies + 1 
+WHERE id=" . $loan['book_id']);
+
+
+$conn->query("UPDATE loans 
+SET status='returned' 
+WHERE id=$loan_id");
+
+header("Location: my_loans.php");
+exit();
+?>
