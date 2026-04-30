50963fc5bc22831b469ed523c76671d0644bd147
BellemManal <bellemmanel@gmail.com>
2026-04-30 22:05:01 +0200

    Create list.php

---
+++ users/list.php
@@ -0,0 +1,25 @@
+<?php
+include("../config/db.php");
+
+$result = $conn->query("SELECT * FROM users");
+?>
+
+<h2> Users List</h2>
+
+<table border="1">
+<tr>
+    <th>Name</th>
+    <th>Email</th>
+    <th>Role</th>
+    <th>Status</th>
+</tr>
+
+<?php while($row = $result->fetch_assoc()): ?>
+<tr>
+    <td><?= $row['name'] ?></td>
+    <td><?= $row['email'] ?></td>
+    <td><?= $row['role'] ?></td>
+    <td><?= $row['active'] ?></td>
+</tr>
+<?php endwhile; ?>
+</table>
