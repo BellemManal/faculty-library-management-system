6970b3c83df3f9881b77078b148f63fd89d8d460
BellemManal <bellemmanel@gmail.com>
2026-04-30 22:40:50 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,7 +1,11 @@
 <?php
 $conn = new mysqli("localhost", "root", "", "library_db");
 
+
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    die(" Connection failed: " . $conn->connect_error);
 }
+
+
+$conn->set_charset("utf8");
 ?>
