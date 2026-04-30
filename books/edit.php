c5d62fcab328c549a64cd2eb1f595af26eb67f87
BellemManal <bellemmanel@gmail.com>
2026-04-30 22:21:03 +0200

    Update edit.php

--- books/edit.php
+++ books/edit.php
@@ -1,4 +1,13 @@
 <?php
+session_start();
+
+if(!isset($_SESSION['user_id'])){
+    header("Location: auth/login.php");
+    exit();
+}
+?>
+
+<?php
 include("../config/db.php");
 
 $id = $_GET['id'];
