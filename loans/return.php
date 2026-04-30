494692f99de5c1fcea9a6e73b75870dea9c9c805
BellemManal <bellemmanel@gmail.com>
2026-04-30 22:31:33 +0200

    Update return.php

--- loans/return.php
+++ loans/return.php
@@ -2,29 +2,34 @@
 session_start();
 
 if(!isset($_SESSION['user_id'])){
-    header("Location: auth/login.php");
+    header("Location: ../auth/login.php");
     exit();
 }
-?>
 
-<?php
 include("../config/db.php");
-session_start();
 
 $loan_id = $_GET['loan_id'];
 
 
 $loan = $conn->query("SELECT * FROM loans WHERE id=$loan_id")->fetch_assoc();
 
+if(!$loan){
+    die("Loan not found");
+}
 
-$conn->query("UPDATE books 
+
+$conn->query("
+UPDATE books 
 SET available_copies = available_copies + 1 
-WHERE id=" . $loan['book_id']);
+WHERE id=" . $loan['book_id']
+);
 
 
-$conn->query("UPDATE loans 
+$conn->query("
+UPDATE loans 
 SET status='returned' 
-WHERE id=$loan_id");
+WHERE id=$loan_id
+");
 
 header("Location: my_loans.php");
 exit();
