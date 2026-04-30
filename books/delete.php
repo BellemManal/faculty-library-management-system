2aca2303c6e271ea31e57a82b8aa691724d2023a
BellemManal <bellemmanel@gmail.com>
2026-04-30 22:36:37 +0200

    Update delete.php

--- books/delete.php
+++ books/delete.php
@@ -1,4 +1,16 @@
 <?php
+session_start();
+
+if(!isset($_SESSION['user_id'])){
+    header("Location: ../auth/login.php");
+    exit();
+}
+
+
+if($_SESSION['role'] != "librarian" && $_SESSION['role'] != "admin"){
+    die(" Access denied");
+}
+
 include("../config/db.php");
 
 $id = $_GET['id'];
@@ -9,7 +21,3 @@ $conn->query("DELETE FROM books WHERE id=$id");
 header("Location: list.php");
 exit();
 ?>
-
-if($_SESSION['role'] != "librarian" && $_SESSION['role'] != "admin"){
-    die("Access denied");
-}
