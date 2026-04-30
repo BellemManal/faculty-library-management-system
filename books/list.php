3962739b3e34185c4d0e1fa4d96b6470c3ca12e8
BellemManal <bellemmanel@gmail.com>
2026-04-30 20:26:29 +0200

    Update list.php

--- books/list.php
+++ books/list.php
@@ -5,22 +5,35 @@ session_start();
 $result = $conn->query("SELECT * FROM books");
 ?>
 
-<h2>Books List</h2>
+<h2>📚 Books List</h2>
 
 <a href="../dashboard.php">⬅ Back</a>
+<br><br>
 
-<table border="1">
+<table border="1" cellpadding="10">
     <tr>
         <th>Title</th>
         <th>Author</th>
-        <th>Available Copies</th>
+        <th>ISBN</th>
+        <th>Total</th>
+        <th>Available</th>
+        <th>Actions</th>
     </tr>
 
     <?php while($row = $result->fetch_assoc()): ?>
     <tr>
-        <td><?php echo $row['title']; ?></td>
-        <td><?php echo $row['author']; ?></td>
-        <td><?php echo $row['available_copies']; ?></td>
+        <td><?= $row['title'] ?></td>
+        <td><?= $row['author'] ?></td>
+        <td><?= $row['isbn'] ?></td>
+        <td><?= $row['total_copies'] ?></td>
+        <td><?= $row['available_copies'] ?></td>
+        <td>
+            <a href="edit.php?id=<?= $row['id'] ?>">Edit</a> |
+            <a href="delete.php?id=<?= $row['id'] ?>">Delete</a>
+        </td>
     </tr>
     <?php endwhile; ?>
 </table>
+
+<br>
+<a href="add.php">➕ Add New Book</a>
